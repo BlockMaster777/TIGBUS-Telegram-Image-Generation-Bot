4828fbf68fa09c3c19274ffd89fdbaee6b29dd89
BlockMaster <145277345+ProgrammProfi@users.noreply.github.com>
2025-08-09 19:28:31 +0300

    Ad start and help commands, add generation info message, add typing status.

--- main.py
+++ main.py
@@ -12,6 +12,18 @@ bot = telebot.TeleBot(TG_TOKEN)
 
 prompt = ""
 
+
+@bot.message_handler(commands=["start"])
+def start(message):
+    bot.send_message(message.chat.id, "Привет! Я могу генерировать изображения совершенно бесплатно!\n/gen <промпт> - сгенерировать\n/help - помощь")
+
+
+@bot.message_handler(commands=["help"])
+def start(message):
+    bot.send_message(message.chat.id, "/gen <промпт> - сгенерировать\n")
+
+
+
 @bot.message_handler(commands=['gen'])
 def handle_gen_config(message):
     global prompt
@@ -29,12 +41,15 @@ def gen_img(call):
     style = call.data
     print(style)
     bot.send_message(call.message.chat.id, f"Промт: {prompt}\n Генерация началась")
+    sending_msg = bot.send_message(call.message.chat.id, "Изображение генерируется..")
+    bot.send_chat_action(call.message.chat.id, 'typing')
     api = FusionBrainAPI('https://api-key.fusionbrain.ai/', TOKEN, SECRET_TOKEN)
     pipeline_id = api.get_pipeline()
     uuid = api.generate(f"{prompt}", pipeline_id, style=style)
     api.check_generation(uuid)
     with open("result.png", "rb") as f:
         image = f.read()
+    bot.delete_message(call.message.chat.id, sending_msg.message_id)
     bot.send_photo(call.message.chat.id, image, "Изображение готово!")
     os.remove("result.png")
 
